b2008845224eb22b79491ed6a56962a8b9c680e4
T-Coro <9517685+tcoro@users.noreply.github.com>
2020-11-28 16:07:35 +0100

    added Login and Register in Navbar

--- server/client/src/components/Navbar/Navbar.tsx
+++ server/client/src/components/Navbar/Navbar.tsx
@@ -5,19 +5,29 @@ import './navbar.css'
 export const Navbar = function () {
     return (
         <>
-            <div className={"navBar"}>
-                <Link to={"/"}>
-                    <button>Home</button>
-                </Link>
-                <Link to={"/farmers"}>
-                    <button>Farmers</button>
-                </Link>
-                <Link to={"/cart"}>
-                    <button>Cart</button>
-                </Link>
-                <Link to={"/settings"}>
-                    <button>Settings</button>
-                </Link>
+            <div className={"navbar"}>
+                <div className={"navbarPages"}>
+                    <Link to={"/"}>
+                        <button>Home</button>
+                    </Link>
+                    <Link to={"/farmers"}>
+                        <button>Farmers</button>
+                    </Link>
+                    <Link to={"/cart"}>
+                        <button>Cart</button>
+                    </Link>
+                </div>
+                <div className={"navbarSettings"}>
+                    <Link to={"/login"}>
+                        <button>Login</button>
+                    </Link>
+                    <Link to={"/register"}>
+                        <button>Register</button>
+                    </Link>
+                    <Link to={"/settings"}>
+                        <button>Settings</button>
+                    </Link>
+                </div>
             </div>
         </>
     )
