570ad9e09b67dab929a564f2eb073a810d1303e5
T-Coro <9517685+tcoro@users.noreply.github.com>
2020-11-28 14:44:04 +0100

    fixed build

--- server/client/src/App.tsx
+++ server/client/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Route, Switch, BrowserRouter} from "react-router-dom";
-import './App.css';
 import {Home} from "./components/Home";
 import {Form} from "./components/example/Form";
 
