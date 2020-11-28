a607b89cc54967ae4c933ab1b6b921c7c5495505
T-Coro <9517685+tcoro@users.noreply.github.com>
2020-11-28 16:42:44 +0100

    added UI for login and register

--- server/client/src/App.tsx
+++ server/client/src/App.tsx
@@ -3,6 +3,8 @@ import {Route, Switch, BrowserRouter} from "react-router-dom";
 import {Home} from "./components/Home";
 import {Form} from "./components/example/Form";
 import {Navbar} from "./components/Navbar/Navbar";
+import {Login} from "./components/Auth/Login";
+import {Register} from "./components/Auth/Register";
 
 function App() {
     return (
@@ -11,6 +13,8 @@ function App() {
             <Switch>
                 <Route path="/" exact component={Home}/>
                 <Route path="/example" exact component={Form}/>
+                <Route path="/login" exact component={Login}/>
+                <Route path="/register" exact component={Register}/>
             </Switch>
         </BrowserRouter>
     );
