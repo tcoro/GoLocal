0ea4aebccdce998014f50e4b08dfcc6413e35820
T-Coro <9517685+tcoro@users.noreply.github.com>
2020-11-29 12:44:50 +0100

    fixed merge problems

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,9 +5,6 @@ import {Form} from "./components/example/Form";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Login} from "./components/Auth/Login";
 import {Register} from "./components/Auth/Register";
-import { SettingsFarmer } from './components/settingsFarmer/SettingsFarmer';
-import { SettingsUser } from './components/settingsUser/SettingsUser';
-import { Cart } from './components/cart/Cart';
 
 function App() {
     return (
@@ -18,9 +15,6 @@ function App() {
                 <Route path="/example" exact component={Form}/>
                 <Route path="/login" exact component={Login}/>
                 <Route path="/register" exact component={Register}/>
-                <Route path="/settingsFarmer" exact component={SettingsFarmer}/>
-                <Route path="/settingsUser" exact component={SettingsUser}/>
-                <Route path="/cart" exact component={Cart}/>
             </Switch>
         </BrowserRouter>
     );
