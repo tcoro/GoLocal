2411994bdf8d6bcc48224f43cae65fdc5adb3631
Peter Sejerman <p.sejerman@gmail.com>
2020-11-29 12:15:39 +0100

    brach restor

---
+++ server/backend/dist/entity/User.d.ts
@@ -0,0 +1,11 @@
+export declare class User {
+    id: number;
+    stamp_created: Date;
+    stamp_updated: Date;
+    email: string;
+    type_id: number;
+    secret_id: number;
+    first_name: string;
+    last_name: string;
+}
+//# sourceMappingURL=User.d.ts.map
---
+++ server/backend/src/index.ts
@@ -0,0 +1,42 @@
+import "reflect-metadata";
+import {createConnection, getRepository} from "typeorm";
+import {getManager} from "typeorm";
+import {User} from "./entity/User";
+import {Secret} from "./entity/Secret";
+
+
+createConnection()
+  .then(async (connection) => {
+    let user = await getManager()
+        .createQueryBuilder(User, "user")
+        .where("user.id = :id", { id: 1 })
+        .getOne();
+        console.log(user);
+  }).catch((error) => console.log(error));
+
+
+/*createConnection({
+
+  type: "mysql",
+  host: "localhost",
+  port: 3306,
+  username: "golocal_svc",
+  password: "golocal",
+  database: "golocal_db",
+
+}).then(async connection => {
+
+
+
+
+}).catch(error => console.log(error));
+*/
+/*
+
+
+  let user = await getManager()
+      .createQueryBuilder(User, "user")
+      .where("user.id = :id", { id: 1 })
+      .getOne();
+
+ */
