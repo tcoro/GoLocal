dc67281045e08767be2d06953becbe5950c445c4
Peter Sejerman <p.sejerman@gmail.com>
2020-11-30 09:39:07 +0100

    getUserByEmail() in backend/src/index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,27 +1,23 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import {createConnection, getConnection} from "typeorm";
+import { ConnectionManager } from 'typeorm';
 import {User} from "./entity/User";
+import {Secret} from "./entity/Secret";
 
-createConnection().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
+function getUserCredentials(email){
+  createConnection().then(async connection => {
+  const user = await getConnection()
+      .createQueryBuilder()
+      .select("user")
+      .from(User, "user")
+      .leftJoinAndSelect("user.secret", "secret")
+      .where("user.email = :email", { email: email })
+      .getOne();
 
-    const user = new User();
+      console.log(user);
+      }).catch(error => console.log(error));
+}
 
-    user.firstName = "Lasse";
-    user.lastName = "Bluten";
-    user.typeId = 2;
-    user.email = "lbluten@aol.com";
-    user.secret = "12345";
-
-    await connection.manager.save(user);
-//(await connection.manager.save(secret);
-    console.log("Saved a new user with id: " + user.id);
-/*
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-    //const user1 = await User.findOneById(1, {relations: })
-    //console.log("Here you can setup and run express/koa/any other framework.");
-*/
-}).catch(error => console.log(error));
+let email = "lbluten@mail.de";
+getUserCredentials(email);
